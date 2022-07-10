2f4df04a22f59f3e8b3cec57bf2e4d1f18a7ccb3
Tom Marshall <tdm.code@gmail.com>
2022-07-10 07:24:46 -0700

    fajita: Switch to device_oneplus_common
    Change-Id: Icb68a2c8871ec1399f04917a5686b651ae6bdbf5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/oneplus/fajita
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # FOD
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oneplus:libudfps_extension.oneplus
+TARGET_SURFACEFLINGER_UDFPS_LIB := //device/oneplus/common:libudfps_extension.oneplus
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
